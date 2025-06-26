176d52291e2a93f04aabaded599e12ce86fd5240
abishekbabuofficial <abistudies03@gmail.com>
2025-06-26 14:33:06 +0530

    added frontend

---
+++ frontendd/src/App.tsx
@@ -0,0 +1,30 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { loadStoredAuth } from "./store/authSlice";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+
+// Initialize auth state from localStorage
+store.dispatch(loadStoredAuth());
+
+const App = () => (
+  <Provider store={store}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+  </Provider>
+);
+
+export default App;
